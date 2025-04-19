a1b503fa8f0dda19e9e58d482c255af180e2d069
juliuscarvajal <juliuscarvajal21@gmail.com>
2025-04-19 11:23:25 +1000

    fix: safari profile pic

--- src/components/profile-picture.tsx
+++ src/components/profile-picture.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export const ProfilePicture = () => {
   return (
-    <Avatar className="size-[80px] md:size-[92px] outline">
+    <Avatar className="size-[80px] md:size-[92px] border-2 border-solid border-current">
       <AvatarImage
         src="/images/profile-pic.jpeg"
         alt="Julius Carvajal profile picture"
