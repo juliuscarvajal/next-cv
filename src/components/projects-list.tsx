bb96409e927b5064f1c3186750fa13e739fa8435
juliuscarvajal <juliuscarvajal21@gmail.com>
2025-04-19 22:35:49 +1000

    fix: projects list mailer

--- src/components/projects-list.tsx
+++ src/components/projects-list.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { Mailer } from "./mailer";
 import { PopupModal } from "./popup-modal";
+import { EmailForm } from "./email-form";
 
 function getCompanyByProjectName(name: Project["name"]) {
   return companies.find((company) => company.name === name);
@@ -87,7 +88,7 @@ const ProjectListItem = ({ project, company }: ProjectListItemProps) => {
         }
         title="Contact me"
       >
-        <Mailer />
+        <EmailForm />
       </PopupModal>
     </div>
   );
