2748af5dbfd66b23a422767a4796f70335f15b84
juliuscarvajal <juliuscarvajal21@gmail.com>
2025-04-25 22:16:19 +1000

    fix: eslint

--- src/features/recaptcha/actions/getCaptchaToken.ts
+++ src/features/recaptcha/actions/getCaptchaToken.ts
@@ -3,6 +3,7 @@
 import { captchaSiteKey } from "../constants/constants";
 
 export async function getCaptchaToken() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const grecaptcha = (window as any).grecaptcha;
   const token = await grecaptcha?.execute(captchaSiteKey, {
     action: "submit",
