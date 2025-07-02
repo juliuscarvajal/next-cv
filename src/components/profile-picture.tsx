3068ba5156a6ff63019467be6349ad87cdc33576
juliuscarvajal <juliuscarvajal21@gmail.com>
2025-07-02 19:38:04 +1000

    chore: update profile pic

--- src/components/profile-picture.tsx
+++ src/components/profile-picture.tsx
@@ -4,7 +4,7 @@ export const ProfilePicture = () => {
   return (
     <Avatar className="size-[80px] md:size-[92px] border-2 border-solid border-current">
       <AvatarImage
-        src="/images/profile-pic.jpeg"
+        src="/images/profile-pic-sketch.jpeg"
         alt="Julius Carvajal profile picture"
       />
       <AvatarFallback>JC</AvatarFallback>
